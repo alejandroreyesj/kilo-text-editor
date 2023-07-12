c2175220772d034918b3ca27e4630be6b5758748
Jahir Reyes <alejandro.reyesj@outlook.com>
2023-07-11 23:39:02 -0400

    Error handling and Sections comments

--- kilo.c
+++ kilo.c
@@ -1,20 +1,32 @@
+/*** Includes ***/
+#include <asm-generic/errno-base.h>
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+
+/*** Data ***/
 struct termios orig_termios;
 
+/*** Terminal ***/
 void die(const char *s) {
-    perror(s);
-    exit(1);
+  perror(s);
+  exit(1);
 }
 
-void disableRawMode() { tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); }
+void disableRawMode() {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+    die("tcsetattr");
+  }
+}
 
 void enableRawMode() {
-  tcgetattr(STDIN_FILENO, &orig_termios);
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+    die("tcsetattr");
+  }
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
@@ -25,14 +37,20 @@ void enableRawMode() {
   raw.c_cc[VMIN] = 0;
   raw.c_cc[VTIME] = 1;
 
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+    die("tcsetattr");
+  }
 }
+
+/*** Init ***/
 
 int main(void) {
   enableRawMode();
   while (1) {
     char c = '\0';
-    read(STDIN_FILENO, &c, 1);
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
+      die("read");
+    }
     if (iscntrl(c)) {
       printf("%d\r\n", c);
     } else {
